bf46fba9459504489466b0ac6640a16bed077833
SOUJANYA ROY <146373786+soujo05@users.noreply.github.com>
2025-10-05 21:40:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,4 @@ load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
-# Optional: for image generation or LinkedIn publishing---not yet made
-STABILITY_API_KEY = os.getenv("STABILITY_API_KEY")
-LINKEDIN_CLIENT_ID = os.getenv("LINKEDIN_CLIENT_ID")
-LINKEDIN_CLIENT_SECRET = os.getenv("LINKEDIN_CLIENT_SECRET")
+
